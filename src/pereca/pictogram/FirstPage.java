ecf60eb76b1d902408239553c02bf95a9ded7ce6
Nemanja <nemanja.m.303@gmail.com>
2019-02-18 16:16:30 +0100

    new pictograms (resources)

--- src/pereca/pictogram/FirstPage.java
+++ src/pereca/pictogram/FirstPage.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class FirstPage extends Activity {
@@ -110,15 +111,17 @@ public class FirstPage extends Activity {
 		this.categories.add("medicina");
 		this.categories.add("povrce");
 		this.categories.add("voce");
+		this.categories.add("emotikon");
 
 		// prepare their length
 		this.rows_numer = new HashMap<String, Integer>();
 		this.rows_numer.put("naslovna", 3);
-		this.rows_numer.put("hrana", 4);
-		this.rows_numer.put("igre", 6);
-		this.rows_numer.put("medicina", 3);
-		this.rows_numer.put("povrce", 2);
-		this.rows_numer.put("voce", 3);
+		this.rows_numer.put("hrana", 5);
+		this.rows_numer.put("igre", 5);
+		this.rows_numer.put("medicina", 5);
+		this.rows_numer.put("povrce", 5);
+		this.rows_numer.put("voce", 5);
+		this.rows_numer.put("emotikon", 5);
 
 		// number of columns per page
 		this.columns_number = 2;
@@ -126,12 +129,12 @@ public class FirstPage extends Activity {
 		// define trasitions
 		this.transition_mapping = new HashMap<String, String>();
 
-		this.transition_mapping.put("naslovna-0-0", "medicina");
+		this.transition_mapping.put("naslovna-0-0", "emotikon");
 		this.transition_mapping.put("naslovna-0-1", "hrana");
-		this.transition_mapping.put("naslovna-1-0", "povrce");
-		this.transition_mapping.put("naslovna-1-1", "voce");
-		this.transition_mapping.put("naslovna-2-0", "igre");
-		this.transition_mapping.put("naslovna-2-1", "unknown");
+		this.transition_mapping.put("naslovna-1-0", "igre");
+		this.transition_mapping.put("naslovna-1-1", "medicina");
+		this.transition_mapping.put("naslovna-2-0", "povrce");
+		this.transition_mapping.put("naslovna-2-1", "voce");
 
 		this.pictogram_click_listener = new OnClickListener() {
 
@@ -140,14 +143,80 @@ public class FirstPage extends Activity {
 
 				String view_tag = (String) view.getTag();
 
+				int position = main_list.getVerticalScrollbarPosition();
+
 				handleClickFrom(view_tag);
 
-				adapter = new PictogramAdapter(getApplicationContext(), R.layout.list_row,
-						R.layout.single_pictogram, adapter_source, this);
-				main_list.setAdapter(adapter);
+				((ArrayAdapter<RowContainer>) main_list.getAdapter()).notifyDataSetChanged();
+
+				main_list.setVerticalScrollbarPosition(position);
 
 			}
 		};
+
+	}
+
+	private void handleClickFrom(String source) {
+
+		// category-row-column
+
+		String[] infos = source.split("-");
+
+		// if click was on mainPage
+		if (infos[0].equals("naslovna")) {
+
+			String next_category = this.transition_mapping.get(source);
+
+			// change list_view dataSource
+			this.adapter_source = this.getCategoryContent(next_category);
+
+			this.adapter = new PictogramAdapter(this, R.layout.list_row, R.layout.single_pictogram,
+					this.adapter_source, this.pictogram_click_listener);
+
+			this.main_list.setAdapter(this.adapter);
+
+			this.current_position = next_category;
+
+		} else {
+
+			if (this.current_selected != null) {
+
+				// remove filter from selected
+
+				this.restorePictogram(this.current_selected);
+
+				this.current_selected = null;
+
+			}
+
+			// set filter to new one
+
+			this.selectPictogram(Integer.parseInt(infos[1]), Integer.parseInt(infos[2]));
+
+			this.current_selected = source;
+
+		}
+
+	}
+
+	private void restorePictogram(String pictogram) {
+
+		String[] infos = pictogram.split("-");
+
+		RowContainer row = this.adapter_source.get(Integer.parseInt(infos[1]));
+
+		RowContainer original_row = ((List<RowContainer>) this.resources.get(infos[0]))
+				.get(Integer.parseInt(infos[1]));
+
+		row.replace(Integer.parseInt(infos[2]), original_row.getSingle_row().get(Integer.parseInt(infos[2])));
+
+	}
+
+	private void selectPictogram(int row, int column) {
+
+		RowContainer row_container = this.adapter_source.get(row);
+
+		row_container.replace(column, this.invertBitmap(row_container.getSingle_row().get(column)));
 
 	}
 
@@ -194,72 +263,6 @@ public class FirstPage extends Activity {
 			// exit from application
 			super.onBackPressed();
 		}
-
-	}
-
-	private void handleClickFrom(String source) {
-
-		// category-row-column
-
-		String[] infos = source.split("-");
-
-		// if click was on mainPage
-		if (infos[0].equals("naslovna")) {
-
-			String next_category = this.transition_mapping.get(source);
-
-			// change list_view dataSource
-			this.adapter_source = this.getCategoryContent(next_category);
-
-			// create new adapter with new dataSource
-			this.adapter = new PictogramAdapter(this, R.layout.list_row, R.layout.single_pictogram,
-					this.adapter_source, this.pictogram_click_listener);
-
-			// set adapter to listView
-			this.main_list.setAdapter(this.adapter);
-
-			this.current_position = next_category;
-
-		} else {
-
-			if (this.current_selected != null) {
-
-				// remove filter from selected
-
-				this.restorePictogram(this.current_selected);
-
-				this.current_selected = null;
-
-			}
-
-			// set filter to new one
-
-			this.selectPictogram(Integer.parseInt(infos[1]), Integer.parseInt(infos[2]));
-
-			this.current_selected = source;
-
-		}
-
-	}
-
-	private void restorePictogram(String pictogram) {
-
-		String[] infos = pictogram.split("-");
-
-		RowContainer row = this.adapter_source.get(Integer.parseInt(infos[1]));
-
-		RowContainer original_row = ((List<RowContainer>) this.resources.get(infos[0]))
-				.get(Integer.parseInt(infos[1]));
-
-		row.replace(Integer.parseInt(infos[2]), original_row.getSingle_row().get(Integer.parseInt(infos[2])));
-
-	}
-
-	private void selectPictogram(int row, int column) {
-
-		RowContainer row_container = this.adapter_source.get(row);
-
-		row_container.replace(column, this.invertBitmap(row_container.getSingle_row().get(column)));
 
 	}
 
